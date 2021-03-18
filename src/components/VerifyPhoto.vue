54b64b7ce15bff4c865bc1159079e7d6888222e6
Putri Darmawan <putri1805@gmail.com>
2021-03-19 01:02:50 +0800

    Add object detection and image classifier

--- src/components/VerifyPhoto.vue
+++ src/components/VerifyPhoto.vue
@@ -1,14 +1,24 @@
 <template>
     <div class = "bg">
         <Head v-bind:id="id"></Head>
-        <label for = "imageLoader" class="ins">Screenshots</label>
+        <label for = "imageLoader" class="ins">Screenshots (Make sure that your caption is visible)</label>
         <input type="file" id ="imageLoader" @change="updateCanvasImage"><br>
-        <p class ="ins">Make sure that your caption is visible</p>
-        <canvas id="imageCanvas" ref="imageCanvas"></canvas>
+        <label for = "image" class = "ins">Upload Photo (the one that you post)</label>
+        <input type="file" id = "image" @change="imageClassifier" />
+        <div class ="left">
+            <p class="ins">Screenshot:</p>
+            <canvas id="imageCanvas" ref="imageCanvas"></canvas><br>
+            <p class="ins">Original Photo:</p>
+            <img v-show="uploadedImage" class="preview" :src="uploadedImage"/>
+        </div>
         <div class = "content">
             <p class= "title"> {{status}} </p>
-            <p class= "title"> Result: </p>
+            <p class= "title"> Caption: </p>
             <p class= "txt">{{result}}</p>
+            <p class= "title"> Label: </p>
+            <p class= "txt" v-show="this.label.length!==0">{{label}}</p>
+            <p class= "title"> Class: </p>
+            <p class= "txt">{{classify}}</p>
             <button class = "btn" v-on:click="check()">Check</button>
             <button class = "btn" v-on:click="cancel()">Cancel</button>
         </div>
@@ -33,7 +43,12 @@ export default {
             status: '',
             hash: this.$route.query.hash,
             result: '',
-            captions: []
+            captions: [],
+            label: [],
+            pic: null,
+            loading: false,
+            classify: null,
+            uploadedImage: '',
         }
     },
     methods: {
@@ -120,15 +135,16 @@ export default {
             reader.onload = () => {
                 var img = new Image();
                 img.onload = function() {
-                self.drawCanvasImage(img)
+                    self.drawCanvasImage(img)
                 }
                 img.src = event.target.result;
             };  
             reader.readAsDataURL(files[0]);       
         },
     
-        drawCanvasImage(img) {
+        drawCanvasImage: function(img) {
             const Tesseract = require('tesseract.js');
+            const cocoSsd = require('@tensorflow-models/coco-ssd');
             var canvas = this.$refs.imageCanvas;
             canvas.width = img.width;
             canvas.height = img.height;
@@ -146,6 +162,68 @@ export default {
                 this.status = ''
             })
             .catch(error => console.log(error))
+
+            cocoSsd.load().then(model => {
+                this.model = model
+                model.detect(img).then((predictions) => {
+                    const font = "16px sans-serif";
+                    ctx.font = font;
+                    ctx.textBaseline = "top";
+                    var res = []
+                    predictions.forEach(prediction => {
+                        const x = prediction.bbox[0];
+                        const y = prediction.bbox[1];
+                        const width = prediction.bbox[2];
+                        const height = prediction.bbox[3];
+                        // Draw the bounding box.
+                        ctx.strokeStyle = "#00FFFF";
+                        ctx.lineWidth = 4;
+                        ctx.strokeRect(x, y, width, height);
+                        // Draw the label background.
+                        ctx.fillStyle = "#00FFFF";
+                        const textWidth = ctx.measureText(prediction.class).width;
+                        const textHeight = parseInt(font, 10); // base 10
+                        ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
+                    });
+
+                    predictions.forEach(prediction => {
+                        const x = prediction.bbox[0];
+                        const y = prediction.bbox[1];
+                        // Draw the text last to ensure it's on top.
+                        ctx.fillStyle = "#000000";
+                        ctx.fillText(prediction.class, x, y);
+                        res.push(prediction.class)
+                    });
+                    console.log('Predictions: ');
+                    console.log(predictions);
+                    this.label = res;
+                })
+            });
+        },
+        imageClassifier: function(e) {
+            this.loading = true
+            const files = e.target.files || e.dataTransfer.files;
+            this.createImage(files[0]);
+            
+        },
+            // Show uploaded images
+        createImage: function(file) {
+            const mobilenet = require('@tensorflow-models/mobilenet');
+            this.classify = []
+            var img = new Image();
+            img.crossOrigin = "anonymous";
+            const reader = new FileReader();
+            reader.onload = e => {
+                this.uploadedImage = e.target.result;
+                img.src = e.target.result;
+                mobilenet.load().then(response => {
+                response.classify(img).then(result => {
+                this.loading = false
+                this.classify = result
+            })
+                })
+            }
+            reader.readAsDataURL(file);
         },
     },
     created() {
@@ -155,6 +233,8 @@ export default {
         let externalScript = document.createElement('script')
         externalScript.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.11/vue.min.js')
         externalScript.setAttribute('src', 'https://unpkg.com/tesseract.js@v2.1.0/dist/tesseract.min.js')
+        externalScript.setAttribute('src', 'https://cdn.jsdelivr.net/npm/@tensorflow-models/mobilenet')
+        externalScript.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/tensorflow/1.2.10/tf.min.js')
         document.head.appendChild(externalScript)
     },
 }
@@ -255,11 +335,22 @@ export default {
         cursor: pointer;
     }
     canvas {
-        float:left;
-        width: 35%;
+        width: 100%;
         height: 70%;
         justify-content: center;
-        margin-left: 8%;
+        margin-left: 9%;
         margin-top: 2%;
+    }
+    .preview {
+        float: left;
+        height: 70%;
+        width: 100%;
+        justify-content: center;
+        margin-left: 9%;
+        margin-top: 2%;
+    }
+    .left {
+        float: left;
+        width: 35%
     }
 </style>
