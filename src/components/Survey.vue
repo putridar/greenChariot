1b0d235746b52c1cf7941df7ee4ecaafe836a914
unknown <baskaran.sakthivel@gmail.com>
2021-03-20 00:55:59 +0800

    Survey form layer 1

--- src/components/Survey.vue
+++ src/components/Survey.vue
@@ -1,6 +1,20 @@
 <template>
     <div class = "bg">
         <Head v-bind:id="id"></Head>
+        <img alt="greenChariot" src="../assets/survey.png">
+        <div class = "content">
+            <p class = "title">How much do you consume on a monthly basis?</p>
+            <p class = "txt1">Electricity</p>
+            <input type="number" placeholder="Electricity bills" v-model="Electricity" required><br>
+            <p class = "txt1">Water Expense</p>
+            <input type="text" placeholder="Water bills" v-model="water" required><br>
+            <p class = "txt1">Ecommerce</p>
+            <input type="text"  placeholder="Number of items shipped" v-model="username" required><br>
+            <p class = "txt1">Food</p>
+            <input type="number" placeholder="Percentage of Homegrown food (ie. 20)" v-model="email" required><br>
+            <button class = "create">Next</button>
+            <p class = "txt"> More accurate Data helps us predict better :) </p>
+        </div>
     </div>
 </template>
 
@@ -69,4 +83,95 @@ export default {
         margin: 10px;
         color: #1C746F;
     }
+    img {
+        float: left;
+        width: 40%;
+        justify-content: center;
+        margin-left: 50px
+    }
+    .content {
+        background-color: #FFFFFF;
+        margin-top: 7%;
+        margin-left: 55%;
+        margin-right: 10%;
+        height: 60%;
+        width: 30%;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        border-radius: 8px;
+        padding: 10px
+    }
+    .title {
+        font-family: Inter;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 40px;
+        line-height: 58px;
+        text-align: center;
+        color: #1C746F;
+        margin: 15px;
+    }
+    .signup {
+        background: #2D8F8A;
+        border-radius: 8px;
+        font-family: Inter;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 36px;
+        display: flex;
+        align-items: center;
+        text-align: center;
+        color: #FFFFFF;
+        float: left;
+        height: 70px;
+        padding: 15px;
+        border: #2D8F8A;
+    }
+    input {
+        font-family: Montserrat;
+        padding: 8px;
+        width: 90%;
+        margin: 2%;
+        font-size: 24px;
+        border-radius: 8px;
+        border: 1px solid #E5E5E5;
+        height: 10%
+    }
+    .create {
+        background: #2D8F8A;
+        border-radius: 8px;
+        border: #2D8F8A;
+        font-family: Montserrat;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 36px;
+        line-height: 44px;
+        text-align: center;
+        color: #FFFFFF;
+        width: 80%;
+        height: 60px;
+        margin: 3%;
+        cursor: pointer;
+    }
+    .txt {
+        font-family: Montserrat;
+        color: #1C746F;
+        text-align: center;
+        font-size: 24px;
+        margin: 10px;
+        cursor: pointer;
+    }
+    .txt1 {
+        font-family: Montserrat;
+        color: #1C746F;
+        text-align: left;
+        font-size: 30px;
+        margin: 7px;
+        margin-left: 4%;
+        cursor: pointer;
+        font-weight: bold;
+
+    }
+    
 </style>
