2d392610184a523508909631101cba66528e1781
jhsoh <sohjh98@gmail.com>
2021-04-03 21:21:30 +0800

    updated editprofile

--- src/components/User/Editprofile.vue
+++ src/components/User/Editprofile.vue
@@ -13,12 +13,16 @@
             <input type="text" placeholder="username" v-model="username" required><br>
             <p class = "txt"> Email </p>
             <input type="text" placeholder="email" v-model="email" required><br>
+            <p class = "txt"> New Password (leave it blank if you do not want to change password) </p>
+            <input type="password" placeholder="Password" v-model="password"><br>
+            <p class = "txt"> Confirm Password </p>
+            <input type="password" placeholder="Confirm Password" v-model="confirmpass"><br>
             <p class="title2">Upload your profile picture below!</p>
             <input type="file" accept="image/*" @change="previewimage">
             <button class="btn2" v-on:click="removepic()">Remove picture</button>
             <p class="title2">Preview Here:</p>
             <template v-if= "this.image!=''"><img alt="profilepic" :src="this.preview" class='imgfluid'><br></template>
-            <p><button class="btn" v-on:click="confirm()">Confirm</button>
+            <p><button class="btn" v-on:click="confirm();updated();">Confirm</button>
             <button class="btn" v-on:click="signout()">Sign out</button></p>
         </div>
     </div>
@@ -38,8 +42,11 @@ export default{
             name:'',
             username:'',
             email:'',
+            oldemail:'',
             image:'',
-            preview:''
+            preview:'',
+            password:null,
+            confirmpass:null
         }
     },
     methods:{
@@ -49,6 +56,7 @@ export default{
                 this.name=snapshot.data().name
                 this.username=snapshot.data().username
                 this.email=snapshot.data().email
+                this.oldemail=snapshot.data().email
             })
         },
         confirm:function(){
@@ -58,17 +66,17 @@ export default{
             db.firestore().collection('users').doc(this.id).update({
                 name:this.name,
                 username:this.username,
-                email:this.email,
                 image:this.image
             }).then(() => {
                 alert("Profile updated successfully!")
-                this.$router.push({name:'Dashboard',query:{id:this.id}})
             })
         }
     },
         signout:function(){
-            alert('You have successfully signed out!')
-            this.$router.push({name:'signin',query:{id:this.id}})
+            db.auth().signOut().then(() => {
+                alert('You have successfully signed out!')
+                this.$router.push({name:'signin',query:{id:this.id}})
+            }).catch(error => console.log(error))
         },
         previewimage:function(event){
             this.preview= URL.createObjectURL(event.target.files[0]);
@@ -85,13 +93,61 @@ export default{
                 alert('Profile picture successfully removed!')
                 location.reload()
             })
-        }
+        },
+        updated:function(){
+            const user=db.auth().currentUser
+            var success=true
+            if (this.oldemail==this.email && this.password!=null && this.password==this.confirmpass){
+                user.updatePassword(this.password).catch(error => {
+                    console.log(error)
+                    alert(error)
+                    success=false
+                }).then(() => {if(success==true){
+                      alert("Password has been changed! Please login again.")
+                      this.$router.push({name:'signin',query:{id:this.id}})
+                }})
+            }else if (this.confirmpass!=null && this.password!=this.confirmpass){
+                alert("Your password confirmation is incorrect!")
+            }else if (this.oldemail!=this.email && this.confirmpass==null){
+                user.updateEmail(this.email).catch(error => {
+                    alert(error)
+                    success=false
+                }).then(() => {
+                    if(success==true){
+                        db.firestore().collection('users').doc(this.id).update({
+                            email:this.email
+                        }).then(() => {
+                            alert("Email successfully updated! Please relogin!")
+                            this.$router.push({name:'signin',query:{id:this.id}})
+                        })
+                    }
+                })
+            } else if (this.oldemail!=this.email && this.confirmpass!=null){
+                user.updateEmail(this.email).catch(error => {
+                    alert(error)
+                    success=false
+                }).then(() => {
+                    if (success){
+                        user.updatePassword(this.password).then(() => {
+                            db.firestore().collection('users').doc(this.id).update({
+                                email:this.email
+                            })
+                        }).then( () => {
+                            alert("Email and Password resetted! Please relogin again")
+                            this.$router.push({name:'signin',query:{id:this.id}})
+                        })
+                    }
+                })
+            }
+                        
+        } 
     },
        
     created(){
         this.retrieve()
     }
     
+
 }
 </script>
 
