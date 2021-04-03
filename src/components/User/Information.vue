380f1a02a1126265272a800f3b5c97571138adb9
Putri Darmawan <59235304+putridar@users.noreply.github.com>
2021-04-04 00:38:58 +0800

    Update Information.vue

--- src/components/User/Information.vue
+++ src/components/User/Information.vue
@@ -39,7 +39,8 @@ export default {
     data() {
         return {
             articles: [],
-            topics: []
+            topics: [],
+            id: this.$route.query.id,
         }
     },
     methods: {
