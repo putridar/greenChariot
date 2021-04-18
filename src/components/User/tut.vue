103ae1aae85bd559c697644f852ce2ad1415f5b0
tetsumiii <deannasengsy@gmail.com>
2021-04-18 16:52:35 +0800

    start of intro page

--- src/components/User/tut.vue
+++ src/components/User/tut.vue
@@ -1,15 +1,19 @@
 <template>
     <div>
         <div class='bg'>
-            <div class='fade-in'>
+            <div class='fade-in one'>
                 <div class='logo'>
                     <p class="greenlogo">green</p>
                     <p class="whitelogo">Chariot</p>
-                    <br>
-                    <div class='slogan'>
-                        <p class='subtitles'>going green has never been easier</p>
-                    </div>
                 </div>
+            </div>
+            <div class='fade-in two'>
+                <div class='slogan'>
+                    <p class='subtitles'>going green has never been easier</p>
+                </div>
+            </div>
+            <div class='fade-in three'> 
+                <button type='button' class='btn'>Begin</button>
             </div>
         </div>
     </div>
@@ -34,61 +38,43 @@ export default{
     @import url('https://fonts.googleapis.com/css?family=Montserrat');
     @import url('https://fonts.googleapis.com/css?family=Inter');
     .fade-in {
-        animation: fadeIn ease 5s;
-        -webkit-animation: fadeIn ease 5s;
-        -moz-animation: fadeIn ease 5s;
-        -o-animation: fadeIn ease 5s;
-        -ms-animation: fadeIn ease 5s;
-        background: linear-gradient(
-            35deg,
-        #77c5fc,
-        #77c5fc
-        );
+        opacity:0;
+        opacity: 1 \9; 
+        -webkit-animation:fadeIn ease-in 1;
+        -moz-animation:fadeIn ease-in 1;
+        animation:fadeIn ease-in 1;
+        -webkit-animation-fill-mode:forwards;
+        -moz-animation-fill-mode:forwards;
+        animation-fill-mode:forwards;
+        -webkit-animation-duration:1s;
+        -moz-animation-duration:1s;
+        animation-duration:1s;
     }
-    @keyframes fadeIn {
-        0% {
-            opacity:0;
-        }
-        100% {
-            opacity:1;
-        } 
+    .fade-in.one {
+        -webkit-animation-delay: 0.1s;
+        -moz-animation-delay: 0.1s;
+        animation-delay: 0.1s;
     }
-    @-moz-keyframes fadeIn {
-        0% {
-            opacity:0;
-        }
-        100% {
-            opacity:1;
-        }
+    .fade-in.two {
+        -webkit-animation-delay: 3s;
+        -moz-animation-delay: 3s;
+        animation-delay: 3s;
     }
-    @-webkit-keyframes fadeIn {
-        0% {
-            opacity:0;
-        }
-        100% {
-            opacity:1;
-        }
+    .fade-in.three {
+        -webkit-animation-delay: 6s;
+        -moz-animation-delay: 6s;
+        animation-delay: 6s;
     }
-    @-o-keyframes fadeIn {
-        0% {
-            opacity:0;
-        }
-        100% {
-            opacity:1;
-        }
-    }
-    @-ms-keyframes fadeIn {
-        0% {
-            opacity:0;
-        }
-        100% {
-            opacity:1;
-        }
-    }
+    @-webkit-keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
+    @-moz-keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
+    @keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
+    @-o-keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
+    @-ms-keyframes fadeIn { from { opacity:0; } to { opacity:1; } 
+}
     .greenlogo {
         font-family: Inter;
         font-weight: bold;
-        font-size: 64px;
+        font-size: 80px;
         line-height: 103px;
         color: #1C746F;
         text-align: center;
@@ -98,19 +84,28 @@ export default{
     .whitelogo {
         font-family: Inter;
         font-weight: bold;
-        font-size: 64px;
+        font-size: 80px;
         line-height: 103px;
         color: #FFFFFF;
         text-align:center;
         display: inline;
         margin-left: 0px;
-    }.subtitles {
+    }
+    .subtitles {
         font-family: Montserrat;
         font-size: 25px;
         line-height: 103px;
         color: #FFFFFF;
         text-align: center;
         display: inline;
+    }
+    .slogan {
+        height: 200px;
+        width: 900px;
+        position:fixed;
+        top: 50%;
+        left: 50%;
+        margin-left: -210px;    
     }
     .bg{
         background-color: #77c5fc;
@@ -126,8 +121,27 @@ export default{
         top: 50%;
         left: 50%;
         margin-top: -100px;
-        margin-left: -200px;
-        border:10px solid black;
+        margin-left: -240px;
+    }
+    .btn {
+        background: #2D8F8A;
+        border-radius: 8px;
+        font-family: Montserrat;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 20px;
+        line-height: 24px;
+        text-align: center;
+        color: #FFFFFF;
+        cursor: pointer;
+        position:fixed;
+        top: 50%;
+        left: 50%;
+        margin-top: 100px;
+        width:300px; 
+        margin-left:-150px;
+        padding:15px;
+        border: solid black 1px;
     }
 
 </style>
