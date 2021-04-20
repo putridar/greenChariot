bab041b74bfe5221d1d566309ec75d9512fa59a6
jhsoh <sohjh98@gmail.com>
2021-04-20 14:19:00 +0800

    merged

--- src/components/User/SignUp.vue
+++ src/components/User/SignUp.vue
@@ -152,16 +152,17 @@ export default {
              if(pattern.test(this.website)==false){
                  alert("Please input a valid URL!")
                  this.correcturl=false
-             }else{
-                 if (this.website.indexOf('https://')==-1 || (this.website.indexOf('.com')==-1 && this.website.indexOf('.sg')==-1)){
-                     alert('Please input a valid URL!')
-                     this.correcturl=false
-                 }
              }
-        }
+            
+            if (this.website.indexOf('https://')==-1 || (this.website.indexOf('.com')==-1 && this.website.indexOf('.sg')==-1)){
+                alert('Please input a valid URL!')
+                this.correcturl=false
+                 }
+             },
+            
+        },
     
     }
-}
 </script>
 
 <style scoped>
